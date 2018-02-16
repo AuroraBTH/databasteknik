51a717986ff2dd5dd6428c5fa77ceefca70bcd8c
MagnusGreiff <mangegreiff@gmail.com>
2018-02-16 10:47:31 +0100

    Configuration base

---
+++ config/route.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Configuration file for routes.
+ */
+return [
+    // Load these routefiles in order specified and optionally mount them
+    // onto a base route.
+    "routeFiles" => [
+        [
+            // These are for internal error handling and exceptions
+            "mount" => null,
+            "file" => __DIR__ . "/route/internal.php",
+        ],
+        [
+            // For debugging and development details on Anax
+            "mount" => "debug",
+            "file" => __DIR__ . "/route/debug.php",
+        ],
+        [
+            // Add routes from userController and mount on user/
+            "mount" => "user",
+            "file" => __DIR__ . "/route/userController.php",
+        ],
+        [
+            // To read flat file content in Markdown from content/
+            "mount" => null,
+            "file" => __DIR__ . "/route/flat-file-content.php",
+        ],
+        [
+            // Keep this last since its a catch all
+            "mount" => null,
+            "file" => __DIR__ . "/route/404.php",
+        ],
+    ],
+
+];
