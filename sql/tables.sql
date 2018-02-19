7085c6c9ce442ab936038773acd532255a305924
Niklas Andersson <nhdandersson@gmail.com>
2018-02-19 16:17:51 +0100

    Updated tables to use new values

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,6 +1,8 @@
 --
 -- Create databasteknik and use it.
 --
+
+DROP DATABASE IF EXISTS databasteknik;
 CREATE DATABASE IF NOT EXISTS databasteknik;
 
 USE databasteknik;
@@ -11,28 +13,17 @@ SET NAMES utf8;
 -- Create the table Category
 --
 
-DROP TABLE IF EXISTS Category;
-CREATE TABLE Category (
+CREATE TABLE IF NOT EXISTS Category (
 	`categoryID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `categoryName` VARCHAR(80) UNIQUE NOT NULL
-) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
-
---
--- Create the table Subcategory
---
-
-DROP TABLE IF EXISTS Subcategory;
-CREATE TABLE Subcategory (
-	`subcategoryID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `subcategoryName` VARCHAR(80) UNIQUE NOT NULL
+    `categoryName` VARCHAR(80) UNIQUE NOT NULL,
+	`parentID` INTEGER DEFAULT NULL
 ) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
 
 --
 -- Create the table Product
 --
 
-DROP TABLE IF EXISTS Product;
-CREATE TABLE Product (
+CREATE TABLE IF NOT EXISTS Product (
 	`productID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `productManufacturer` VARCHAR(80) NOT NULL,
     `productName` VARCHAR(80) NOT NULL,
@@ -43,80 +34,75 @@ CREATE TABLE Product (
     `productBuyPrize` INTEGER NOT NULL,
     `productColor` VARCHAR(20) NOT NULL,
     `productAmount` INTEGER,
-    `productCategory` INTEGER,
-    `productSubcategory` INTEGER
+    `productCategoryID` INTEGER,
+	FOREIGN KEY (`productCategoryID`) REFERENCES Category(`categoryID`)
 ) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
 
 --
--- Create the table Customer
+-- Create the table Role
 --
 
-DROP TABLE IF EXISTS Customer;
-CREATE TABLE Customer (
-	`customerID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `customerFirstName` VARCHAR(40) NOT NULL,
-    `customerSurName` VARCHAR(80) NOT NULL,
-    `customerPhone` VARCHAR(40),
-    `customerMail` VARCHAR(80) NOT NULL,
-    `customerGender` INTEGER,
-    `customerAddress` VARCHAR (120) NOT NULL,
-    `customerMailingAddress` INTEGER NOT NULL,
-    `customerCity` VARCHAR(80) NOT NULL
+CREATE TABLE IF NOT EXISTS Role (
+	`roleID` INTEGER PRIMARY KEY NOT NULL,
+	`roleName` VARCHAR(80) NOT NULL
 ) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
 
 --
--- Create the table Buyer
+-- Create the table User
 --
 
-CREATE TABLE Buyer (
-	`buyerID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `buyerFirstName` VARCHAR(40) NOT NULL,
-    `buyerSurName` VARCHAR(80) NOT NULL,
-    `buyerPhone` VARCHAR(40),
-    `buyerMail` VARCHAR(80) NOT NULL,
-    `buyerGender` INTEGER,
-    `buyerAddress` VARCHAR (120) NOT NULL,
-    `buyerMailingAddress` INTEGER NOT NULL,
-    `buyerCity` VARCHAR(80) NOT NULL
-) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
-
---
--- Create the table Management
---
-
-CREATE TABLE Management (
-	`managementID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `managementFirstName` VARCHAR(40) NOT NULL,
-    `managementSurName` VARCHAR(80) NOT NULL,
-    `managementPhone` VARCHAR(40),
-    `managementMail` VARCHAR(80) NOT NULL,
-    `managementGender` INTEGER,
-    `managementAddress` VARCHAR (120) NOT NULL,
-    `managementMailingAddress` INTEGER NOT NULL,
-    `managementCity` VARCHAR(80) NOT NULL
-) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
-
---
--- Create the table Purchase
--- WORK IN PROGRESS
---
-
-DROP TABLE IF EXISTS Purchase;
-CREATE TABLE Purchase (
-	`purchaseID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    `customerID` INTEGER NOT NULL,
-    `purchaseTime` DATETIME
+CREATE TABLE IF NOT EXISTS User (
+	`userID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `userFirstName` VARCHAR(40) NOT NULL,
+    `userSurName` VARCHAR(80) NOT NULL,
+    `userPhone` VARCHAR(40),
+    `userMail` VARCHAR(80) NOT NULL,
+    `userGender` INTEGER,
+    `userAddress` VARCHAR (120) NOT NULL,
+    `userMailingAddress` INTEGER NOT NULL,
+    `userCity` VARCHAR(80) NOT NULL,
+	`userRole` INTEGER NOT NULL DEFAULT 0,
+	FOREIGN KEY (`userRole`) REFERENCES Role(`roleID`)
 ) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
 
 --
 -- Create the table Coupons
 --
 
-DROP TABLE IF EXISTS Coupon;
-CREATE TABLE Coupon (
+CREATE TABLE IF NOT EXISTS Coupon (
 	`couponID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `couponName` VARCHAR(20) UNIQUE,
     `couponAmount` INTEGER,
     `startDate` DATETIME,
     `finishDate` DATETIME
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+
+--
+-- Create the table Orders
+-- https://github.com/canax/database/issues/1#issuecomment-338931447
+--
+
+CREATE TABLE IF NOT EXISTS Orders (
+	`orderID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `userID` INTEGER NOT NULL,
+    `purchaseTime` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	`sentTime` DATETIME NULL,
+	`couponID` INTEGER,
+	`orderStatus` VARCHAR(40),
+	FOREIGN KEY (`userID`) REFERENCES User(`userID`),
+	FOREIGN KEY (`couponID`) REFERENCES Coupon(`couponID`)
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+--
+-- Create the table OrderItem
+--
+
+CREATE TABLE IF NOT EXISTS OrderItem (
+	`orderID` INTEGER NOT NULL,
+	`productID` INTEGER NOT NULL,
+	`productAmount` INTEGER NOT NULL,
+	PRIMARY KEY (`orderID`, `productID`),
+	FOREIGN KEY (`orderID`) REFERENCES Orders(`orderID`),
+	FOREIGN KEY (`productID`) REFERENCES Product(`productID`)
 ) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
