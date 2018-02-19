21570557ef821e5f04aa9ab313086e9ab3d8332d
MagnusGreiff <mangegreiff@gmail.com>
2018-02-19 15:29:33 +0100

    Product and ProductController Base

---
+++ src/Product/Product.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Course\Product;
+
+use \Anax\DI\InjectionAwareInterface;
+use \Anax\Di\InjectionAwareTrait;
+
+class Product implements InjectionAwareInterface {
+    use InjectionAwareTrait;
+
+    # TODO: Implement functions to handle frontpage actions.
+
+}
