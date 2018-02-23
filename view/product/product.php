190de2cfbc3c763ab8bdaba8e98df86b872f56f3
Niklas Andersson <nhdandersson@gmail.com>
2018-02-23 16:34:46 +0100

    * Simple view to display one product (by ID).

--- view/product/product.php
+++ view/product/product.php
@@ -3,6 +3,34 @@ namespace Anax\View;
 ?>
 
 <div class="home">
-    <p>Placeholder product. Will Fix later when real content exists</p>
-    <p>Current id: <?= $data["productId"] ?></p>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Tillverkare</th>
+                <th scope="col">Namn</th>
+                <th scope="col">Ursprungsland</th>
+                <th scope="col">Vikt</th>
+                <th scope="col">Storlek</th>
+                <th scope="col">Pris</th>
+                <th scope="col">Färg</th>
+                <th scope="col">Antal</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($data as $item): ?>
+                <tr>
+                    <th scope="row"><?php print_r($item->productID); ?></th>
+                    <td><?php print_r($item->productManufacturer); ?></td>
+                    <td><?php print_r($item->productName); ?></td>
+                    <td><?php print_r($item->productOriginCountry); ?></td>
+                    <td><?php print_r($item->productWeight); ?></td>
+                    <td><?php print_r($item->productSize); ?></td>
+                    <td><?php print_r($item->productSellPrize); ?></td>
+                    <td><?php print_r($item->productColor); ?></td>
+                    <td><?php print_r($item->productAmount); ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
