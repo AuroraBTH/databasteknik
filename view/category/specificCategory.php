7390307b7f5b264bfb2d92c993cbda70ad28e1a4
MagnusGreiff <mangegreiff@gmail.com>
2018-02-23 13:17:23 +0100

    renamed id to categoryId

--- view/category/specificCategory.php
+++ view/category/specificCategory.php
@@ -4,5 +4,5 @@ namespace Anax\View;
 
 <div class="home">
     <p>Placeholder specificCategory. Will Fix later when real content exists</p>
-    <p>Current id: <?= $data["id"] ?></p>
+    <p>Current id: <?= $data["categoryId"] ?></p>
 </div>
