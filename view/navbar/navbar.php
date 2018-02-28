f70696628b1d5bfbaf76c44900f60c642ba00e7e
Niklas Andersson <nhdandersson@gmail.com>
2018-02-28 16:50:04 +0100

    Updated navbar design.

--- view/navbar/navbar.php
+++ view/navbar/navbar.php
@@ -1,5 +1,7 @@
-<nav class="navigation">
-    <ul class="navigationUl">
-        <?php echo $di->get('navbar')->createNav()?>
-    </ul>
+<nav class="navbar navbar-expand-md navbar-light bg-light  border border-right-0 border-left-0 border-top-0">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav d-flex flew-row justify-content-around w-100">
+            <?php echo $di->get('navbar')->createNav()?>
+        </ul>
+    </div>
 </nav>
