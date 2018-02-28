040d73ec2d84cec9a2413e277a6aa409d84387f9
MagnusGreiff <mangegreiff@gmail.com>
2018-02-28 09:46:28 +0100

    Removed all commented code

--- config/url.php
+++ config/url.php
@@ -2,13 +2,4 @@
 /**
  * Config file for url.
  */
-return [
-    // Defaults to use when creating urls.
-    //"siteUrl"       => null,
-    //"baseUrl"       => null,
-    //"staticSiteUrl" => null,
-    //"staticBaseUrl" => null,
-    //"scriptName"    => null,
-    // "urlType"       => \Anax\Url\Url::URL_CLEAN,
-    //"urlType"       => \Anax\Url\Url::URL_APPEND,
-];
+return [];
