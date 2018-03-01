5a32351d35617cb2a250e889e0ed54a5b33b040e
MagnusGreiff <mangegreiff@gmail.com>
2018-03-01 18:20:22 +0100

    Removed unnecessary links from navigation

--- config/navbar.php
+++ config/navbar.php
@@ -14,18 +14,6 @@ return [
             "category" => [
                 "text" => "Kategori",
                 "route" => "category"
-            ],
-            "specificCategory" => [
-                "text" => "SKategori",
-                "route" => "category/1"
-            ],
-            "specificSubCategory" => [
-                "text" => "SSKategori",
-                "route" => "category/2/3"
-            ],
-            "product" => [
-                "text" => "Product",
-                "route" => "product/1"
             ]
         ],
     ],
