f11c85cc7bba9301bca998c08e7872212191321d
MagnusGreiff <mangegreiff@gmail.com>
2018-03-01 14:39:29 +0100

    Updated view to render categories

--- view/category/categories.php
+++ view/category/categories.php
@@ -1,3 +1,25 @@
+<?php
+namespace Anax\View;
+
+$categoryUrl = url("category");
+
+?>
+
 <div class="home">
-    <p>Placeholder category. Will Fix later when real content exists</p>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Kategori</th>
+                <th scope="col">Kön</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($data["categories"] as $category) : ?>
+                <tr>
+                     <td><a href="<?= $categoryUrl ?>/<?= $category->categoryID ?>"><?= $category->categoryName; ?></a></td>
+                     <td><?= $category->gender === 0 ? "Kvinna" : "Man"; ?> </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </div>
