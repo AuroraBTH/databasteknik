cd968f643e66400a451a068db60f0d2a58b52a61
Niklas Andersson <nhdandersson@gmail.com>
2018-03-08 14:49:34 +0100

    Another fix for Codacy

--- sql/dummy.sh
+++ sql/dummy.sh
@@ -9,7 +9,7 @@ dummyCategoryName=('Byxa' 'Tröja' 'Jacka' 'Underklädesplagg' 'Klänning' 'Väs
 dummyManufacturer=('HM' 'Cubus' 'GANT' 'Jack&Jones' 'Levi' 'Adrian Hammond')
 dummytOriginCountry=('Sverige' 'Danmark' 'Finland' 'Norge' 'Tyskland' 'Nya Zeeland')
 dummyColor=('Rosa' 'Lila' 'Grön' 'Blå' 'Gul' 'Ljusblå' 'Vit' 'Svart')
-dummyComplete=('INSERT INTO Product(`productManufacturer`, `productName`, `productOriginCountry`, `productWeight`, `productSize`, `productSellPrize`, `productBuyPrize`, `productColor`, `productAmount`, `productCategoryID`) VALUES' )
+dummyComplete=("INSERT INTO Product(`productManufacturer`, `productName`, `productOriginCountry`, `productWeight`, `productSize`, `productSellPrize`, `productBuyPrize`, `productColor`, `productAmount`, `productCategoryID`) VALUES ")
 #COUNTERALL=0
 for _ in $(seq 1 ${amount});
 do
