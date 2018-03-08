0b1d9f9ff2181a2d4a2815201d3ce800b044687f
Niklas Andersson <nhdandersson@gmail.com>
2018-03-08 14:56:06 +0100

    Updated dummy data script. (#40)
    Updated dummy data script.

--- sql/dummy.sh
+++ sql/dummy.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
-dummySize=(SX S M L XL XXL)
+echo "How many products do you want? (1+) (Press Enter to continue)"
+read amount
+
+dummySize=(XS S M L XL XXL)
 dummyCategory=(1 2 3 4 5 6 7 8 9 10)
-dummyManufacturer=('HM' 'Cubus' 'QM' 'dummy1' 'dummy2')
-dummytOriginCountry=('Sweden' 'Denmark' 'Finland' 'Norway' 'Germany' 'New Zealand')
-dummyColor=('Pink' 'Purple' 'Green' 'Blue' 'Yellow' 'Cyan')
-dummyComplete=()
+dummyCategoryName=('Byxa' 'Tröja' 'Jacka' 'Underklädesplagg' 'Klänning' 'Väska' 'Byxa' 'Tröja' 'Jacka' 'Underklädesplagg' 'Väska')
+dummyManufacturer=('HM' 'Cubus' 'GANT' 'Jack&Jones' 'Levi' 'Adrian Hammond')
+dummytOriginCountry=('Sverige' 'Danmark' 'Finland' 'Norge' 'Tyskland' 'Nya Zeeland')
+dummyColor=('Rosa' 'Lila' 'Grön' 'Blå' 'Gul' 'Ljusblå' 'Vit' 'Svart')
+dummyComplete=("INSERT INTO Product(`productManufacturer`, `productName`, `productOriginCountry`, `productWeight`, `productSize`, `productSellPrize`, `productBuyPrize`, `productColor`, `productAmount`, `productCategoryID`) VALUES ")
 #COUNTERALL=0
-for _ in $(seq 1 2000);
+for _ in $(seq 1 ${amount});
 do
     RANDOMSIZE=$(( ( RANDOM % 6 ) ))
-    RANDOMCATEGORY=$(( ( RANDOM % 3 ) ))
-    RANDOMMANUFACTURER=$(( ( RANDOM % 5 ) ))
+    RANDOMCATEGORY=$(( ( RANDOM % 10 ) ))
+    RANDOMMANUFACTURER=$(( ( RANDOM % 6 ) ))
     RANDOMORIGIN=$(( ( RANDOM % 6 ) ))
-    RANDOMWEIGHT=$(( ( RANDOM % 1000) + 200 ))
-    RANDOMSELLPRIZE=$(( ( RANDOM % 2000) + 70 ))
-    RANDOMBUYPRIZE=$(( ( RANDOM % 1000) + 70 ))
-    RANDOMCOLOR=$(( ( RANDOM % 6 ) ))
+    RANDOMWEIGHT=$(( ( RANDOM % 1000 ) + 200 ))
+    RANDOMBUYPRIZE=$(( ( RANDOM % 1000 ) + 70 ))
+    RANDOMSELLPRIZE=$(( ( RANDOM % 2000 ) + $RANDOMBUYPRIZE ))
+    RANDOMCOLOR=$(( ( RANDOM % 8 ) ))
     RANDOMAMOUNT=$(( ( RANDOM % 200 ) + 1 ))
 
     dummyComplete+=("
-    ${dummyManufacturer[RANDOMMANUFACTURER]},
-    'En fin insert here',
-    ${dummytOriginCountry[RANDOMORIGIN]},
-    ${RANDOMWEIGHT}, ${dummySize[RANDOMSIZE]},
-    ${RANDOMSELLPRIZE}, ${RANDOMBUYPRIZE},
-    ${dummyColor[RANDOMCOLOR]},
+    ("\"${dummyManufacturer[RANDOMMANUFACTURER]}"\",
+    "\"${dummyManufacturer[RANDOMMANUFACTURER]} ${dummyColor[RANDOMCOLOR]} ${dummyCategoryName[RANDOMCATEGORY]}"\",
+    "\"${dummytOriginCountry[RANDOMORIGIN]}"\",
+    ${RANDOMWEIGHT},
+    "\"${dummySize[RANDOMSIZE]}"\",
+    ${RANDOMSELLPRIZE},
+    ${RANDOMBUYPRIZE},
+    "\"${dummyColor[RANDOMCOLOR]}"\",
     ${RANDOMAMOUNT},
-    ${dummyCategory[RANDOMCATEGORY]}
+    ${dummyCategory[RANDOMCATEGORY]}),
     ")
     #echo The counter is $COUNTERALL
     #let COUNTERALL=COUNTERALL+1
 done
+
+
+echo ${dummyComplete[@]} | sed -e '$s/,$//' > dummy_data.sql
