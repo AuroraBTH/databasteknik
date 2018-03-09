3adef03a5c038fa10f03d7fc1b13ffc820d6c94e
Niklas Andersson <nhdandersson@gmail.com>
2018-03-09 09:34:47 +0100

    Added subcategories * Updated dummy.sh to add to a random category (not main ones). * Updated dummy.sh to use that random category name as part of its own name.

--- sql/dummy.sh
+++ sql/dummy.sh
@@ -4,17 +4,18 @@ echo "How many products do you want? (1+) (Press Enter to continue)"
 read amount
 
 dummySize=(XS S M L XL XXL)
-dummyCategory=(1 2 3 4 5 6 7 8 9 10)
-dummyCategoryName=('Byxa' 'Tröja' 'Jacka' 'Underklädesplagg' 'Klänning' 'Väska' 'Byxa' 'Tröja' 'Jacka' 'Underklädesplagg' 'Väska')
+dummyCategoryName=('' '' '' '' '' '' '' '' '' '' '' '' 'Shorts' 'Jeans' 'Träningsbyxor' 'Trosor' 'BH' 'Nattlinnen' 'T-shirt' 'Sweatshirt' 'Cardigans' 'Linnen' 'Långklänningar' 'Korta klänningar' 'Vinterjackor' 'Skinnjackor' 'Övriga jackor' 'Skinnväskor' 'Ryggsäckar' 'Shorts' 'Jeans' 'Träningsbyxor' 'T-shirt' 'Sweatshirt' 'Cardigans' 'Linnen' 'Y-front' 'Boxershort' 'Vinterjackor' 'Skinnjackor' 'Övriga jackor' 'Kostymer' 'Skinnväskor' 'Ryggsäckar')
 dummyManufacturer=('HM' 'Cubus' 'GANT' 'Jack&Jones' 'Levi' 'Adrian Hammond')
 dummytOriginCountry=('Sverige' 'Danmark' 'Finland' 'Norge' 'Tyskland' 'Nya Zeeland')
 dummyColor=('Rosa' 'Lila' 'Grön' 'Blå' 'Gul' 'Ljusblå' 'Vit' 'Svart')
 dummyComplete=('INSERT INTO Product(`productManufacturer`, `productName`, `productOriginCountry`, `productWeight`, `productSize`, `productSellPrize`, `productBuyPrize`, `productColor`, `productAmount`, `productCategoryID`) VALUES ')
-#COUNTERALL=0
+
+
 for _ in $(seq 1 ${amount});
 do
     RANDOMSIZE=$(( ( RANDOM % 6 ) ))
-    RANDOMCATEGORY=$(( ( RANDOM % 10 ) ))
+    RANDOMCATEGORY=$(( ( RANDOM % 32 ) + 12 ))
+    RANDOMCATEGORYID=$(( ( $RANDOMCATEGORY )))
     RANDOMMANUFACTURER=$(( ( RANDOM % 6 ) ))
     RANDOMORIGIN=$(( ( RANDOM % 6 ) ))
     RANDOMWEIGHT=$(( ( RANDOM % 1000 ) + 200 ))
@@ -33,10 +34,12 @@ do
     ${RANDOMBUYPRIZE},
     "\"${dummyColor[RANDOMCOLOR]}"\",
     ${RANDOMAMOUNT},
-    ${dummyCategory[RANDOMCATEGORY]}),
+    ${RANDOMCATEGORY}),
     ")
     #echo The counter is $COUNTERALL
     #let COUNTERALL=COUNTERALL+1
+
+    # echo $RANDOMCATEGORY >> test.txt
 done
 
 echo ${dummyComplete[@]} | sed -e '$s/,$//' > dummy_data.sql
---
+++ sql/uber.sql
@@ -0,0 +1,213 @@
+--
+-- Create databasteknik and use it.
+--
+
+DROP DATABASE IF EXISTS databasteknik;
+CREATE DATABASE IF NOT EXISTS databasteknik;
+
+USE databasteknik;
+
+SET NAMES utf8;
+
+--
+-- Create the table Category
+--
+
+CREATE TABLE IF NOT EXISTS Category (
+	`categoryID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `categoryName` VARCHAR(80) NOT NULL,
+	`parentID` INTEGER DEFAULT NULL,
+	`gender`   INTEGER NOT NULL
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+--
+-- Create the table Product
+--
+
+CREATE TABLE IF NOT EXISTS Product (
+	`productID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `productManufacturer` VARCHAR(80) NOT NULL,
+    `productName` VARCHAR(80) NOT NULL,
+    `productOriginCountry` VARCHAR(40) NOT NULL,
+    `productWeight` INTEGER NOT NULL,
+    `productSize` VARCHAR(3) NOT NULL,
+    `productSellPrize` INTEGER NOT NULL,
+    `productBuyPrize` INTEGER NOT NULL,
+    `productColor` VARCHAR(20) NOT NULL,
+    `productAmount` INTEGER,
+    `productCategoryID` INTEGER,
+	FOREIGN KEY (`productCategoryID`) REFERENCES Category(`categoryID`)
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+--
+-- Create the table Role
+--
+
+CREATE TABLE IF NOT EXISTS Role (
+	`roleID` INTEGER PRIMARY KEY NOT NULL,
+	`roleName` VARCHAR(80) NOT NULL
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+--
+-- Create the table User
+--
+
+CREATE TABLE IF NOT EXISTS User (
+	`userID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `userFirstName` VARCHAR(40) NOT NULL,
+    `userSurName` VARCHAR(80) NOT NULL,
+    `userPhone` VARCHAR(40),
+    `userMail` VARCHAR(80) NOT NULL,
+    `userGender` INTEGER,
+    `userAddress` VARCHAR (120) NOT NULL,
+    `userPostcode` INTEGER NOT NULL,
+    `userCity` VARCHAR(80) NOT NULL,
+	`userRole` INTEGER NOT NULL DEFAULT 0,
+	`userPassword` VARCHAR(255) NOT NULL,
+	FOREIGN KEY (`userRole`) REFERENCES Role(`roleID`)
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+--
+-- Create the table Coupons
+--
+
+CREATE TABLE IF NOT EXISTS Coupon (
+	`couponID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `couponName` VARCHAR(20) UNIQUE,
+    `couponAmount` INTEGER,
+    `startDate` DATETIME,
+    `finishDate` DATETIME
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+
+--
+-- Create the table Orders
+-- https://github.com/canax/database/issues/1#issuecomment-338931447
+--
+
+CREATE TABLE IF NOT EXISTS Orders (
+	`orderID` INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `userID` INTEGER NOT NULL,
+    `purchaseTime` DATETIME DEFAULT CURRENT_TIMESTAMP,
+	`sentTime` DATETIME NULL,
+	`couponID` INTEGER,
+	`orderStatus` VARCHAR(40),
+	FOREIGN KEY (`userID`) REFERENCES User(`userID`),
+	FOREIGN KEY (`couponID`) REFERENCES Coupon(`couponID`)
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+--
+-- Create the table OrderItem
+--
+
+CREATE TABLE IF NOT EXISTS OrderItem (
+	`orderID` INTEGER NOT NULL,
+	`productID` INTEGER NOT NULL,
+	`productAmount` INTEGER NOT NULL,
+	PRIMARY KEY (`orderID`, `productID`),
+	FOREIGN KEY (`orderID`) REFERENCES Orders(`orderID`),
+	FOREIGN KEY (`productID`) REFERENCES Product(`productID`)
+) ENGINE INNODB CHARACTER SET utf8 COLLATE utf8_swedish_ci;
+
+USE databasteknik;
+
+SET NAMES utf8;
+
+INSERT INTO Category (
+    `categoryName`,
+    `gender`
+) VALUES ("Byxor", 0), ("Tröjor", 0), ("Jackor", 0), ("Underkläder", 0), ("Klänningar", 0), ("Väskor", 0), ("Byxor", 1), ("Tröjor", 1), ("Underkläder", 1), ("Jackor", 1), ("Väskor", 1);
+
+INSERT INTO Product(
+    `productManufacturer`,
+    `productName`,
+    `productOriginCountry`,
+    `productWeight`,
+    `productSize`,
+    `productSellPrize`,
+    `productBuyPrize`,
+    `productColor`,
+    `productAmount`,
+    `productCategoryID`
+) VALUES ("HM", "En fin tröja", "Sweden", 200, "M", 200, 100, "Lila", 100, 1);
+
+INSERT INTO Role (
+    `roleID`,
+    `roleName`
+) VALUES (0, "admin");
+
+INSERT INTO User (
+    `userFirstName`,
+    `userSurName`,
+    `userMail`,
+    `userAddress`,
+    `userPostcode`,
+    `userCity`,
+    `userRole`,
+    `userPassword`
+) VALUES ("Carl", "Svensson", "carl.svensson@outlook.com", "Drottningsgatan 13", 37140, "Karlskrona", 0, 'hej123');
+
+INSERT INTO Coupon (
+    `couponName`,
+    `couponAmount`
+) VALUES ("TISDAG", 20);
+
+INSERT INTO Orders (
+    `userID`,
+    `couponID`
+) VALUES (1, 1);
+
+INSERT INTO OrderItem (
+    `orderID`,
+    `productID`,
+    `productAmount`
+) VALUES (1, 1, 2);
+
+USE databasteknik;
+
+SET NAMES utf8;
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Shorts", 0, 1);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Jeans", 0, 1);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Träningsbyxor", 0, 1);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Trosor", 0, 4);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("BH", 0, 4);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Nattlinnen", 0, 4);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("T-shirt", 0, 2);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Sweatshirt", 0, 2);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Cardigans", 0, 2);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Linnen", 0, 2);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Långklänningar", 0, 5);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Korta klänningar", 0, 5);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Vinterjackor", 0, 3);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Skinnjackor", 0, 3);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Övriga jackor", 0, 3);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Skinnväskor", 0, 6);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Ryggsäckar", 0, 6);
+
+-- --
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Shorts", 1, 7);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Jeans", 1, 7);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Träningsbyxor", 1, 7);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("T-shirt", 1, 8);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Sweatshirt", 1, 8);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Cardigans", 1, 8);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Linnen", 1, 8);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Y-front", 1, 9);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Boxershort", 1, 9);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Vinterjackor", 1, 10);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Skinnjackor", 1, 10);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Övriga jackor", 1, 10);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Kostymer", 1, 10);
+
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Skinnväskor", 1, 11);
+INSERT INTO Category (`categoryName`, `gender`, `parentID`) VALUES ("Ryggsäckar", 1, 11);
