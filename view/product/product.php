81819070140ca7830ec85922773a9df88f3fa937
MagnusGreiff <mangegreiff@gmail.com>
2018-03-09 12:59:23 +0100

    Added ajax

--- view/product/product.php
+++ view/product/product.php
@@ -1,13 +1,42 @@
 <?php
 namespace Anax\View;
 ?>
+
+<script>
+    addToCart = () => {
+        var url = window.location.protocol + "//" + window.location.host;
+        var pathArray = window.location.pathname.split( '/' );
+        var newPathname = "";
+            for (i = 0; i < pathArray.length - 2; i++) {
+              newPathname += pathArray[i];
+              newPathname += "/";
+        }
+        var newURL = url + newPathname;
+        $.ajax({
+            type: 'POST',
+            url: newURL + "ajax",
+            dataType: 'text',
+            data: {data: <?php echo(json_encode($data[0])); ?>},
+            success: function() {
+                document.getElementById("addToCartComplete").innerHTML = "Produkten finns nu i din kundvagn";
+                setTimeout(function(){
+                    document.getElementById("addToCartComplete").innerHTML = "";
+                }, 2000);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                alert("Status: " + textStatus); alert("Error: " + errorThrown);
+            }
+        });
+    }
+</script>
+
 <div class="d-flex flex-row justify-content-center p-2">
     <div class="home w-75">
         <div class="d-flex flex-row space-around">
             <div class="p-2 w-50">
                 <img class="img-fluid border" src="https://goo.gl/nVA65Q"></div>
             <div class="p-2 w-50 align-content-center">
-                <div class="d-flex flex-column justify-content-between h-100 bg-light border h-25">
+                <div class="d-flex flex-column justify-content-between h-100 bg-light border h-25 mh-10">
                     <div class="p-3 mb-2 bg-light border text-center border-right-0 border-left-0 border-top-0">
                         <h2><?= $data[0]->productName ?></h2>
                     </div>
@@ -25,7 +54,8 @@ namespace Anax\View;
                     <div class="pt-4 p-3 bg-light border text-center border-right-0 border-left-0 border-bottom-0 h-50">
                         <h3>Pris:</h3>
                         <p class="font-weight-bold"><?= ($data[0]->productSellPrize); ?> kr</p>
-                        <button type="button" class="btn btn-primary w-50">Lägg i kundvagn</button>
+                        <button type="button" onclick="addToCart();" class="btn btn-primary w-50">Lägg i kundvagn</button>
+                        <p id="addToCartComplete"></p>
                     </div>
                 </div>
             </div>
