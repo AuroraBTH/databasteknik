ed2e4391fc0fe38fe39a1f16b4df2f51527b0e9a
Niklas Andersson <nhdandersson@gmail.com>
2018-03-09 10:50:13 +0100

    Now displays all subcategories to a specific category.

--- view/category/specificCategory.php
+++ view/category/specificCategory.php
@@ -1,8 +1,29 @@
 <?php
 namespace Anax\View;
+
+$numOfCols = 3;
+$rowCount = 0;
+$bootstrapColWidth = 12 / $numOfCols;
+
+$productUrl = url("products");
+$gender = $data["title"][0]->gender == 0 ? 'Dam' : 'Herr';
 ?>
 
-<div class="home">
-    <p>Placeholder specificCategory. Will Fix later when real content exists</p>
-    <p>Current id: <?= $data["categoryId"] ?></p>
+<div class="d-flex flex-row justify-content-center p-2">
+    <div class="home w-75">
+        <p class="h2 text-center mt-4"><?= $data["title"][0]->categoryName ?> (<?= $gender ?>)</p>
+        <div class="row text-center mr-0 ml-0">
+            <?php foreach ($data["categories"] as $category) : ?>
+                <div class="w-50 mt-3 mb-3 col-md-<?php echo $bootstrapColWidth; ?>">
+                    <a type="button" class="mx-auto w-75 btn btn-lg btn-primary pt-4 pb-4 btn-block"
+                    href="<?= $productUrl ?>/<?= $category->categoryID ?>"><?= $category->categoryName; ?></a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
+
+<?php
+    $rowCount++;
+    if ($rowCount % $numOfCols == 0) echo '</div><div class="row">';
+?>
