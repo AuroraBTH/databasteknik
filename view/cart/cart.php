0c9b570279870c732bada78fbacd0c729d49c535
MagnusGreiff <mangegreiff@gmail.com>
2018-03-15 16:11:11 +0100

    Removed usage of print_r and replaced it with shortcode.

--- view/cart/cart.php
+++ view/cart/cart.php
@@ -8,6 +8,8 @@ $price = 0;
 $amountOfItems = 0;
 $totalWeight = 0;
 $totalShipping = 0;
+
+$checkout = url("cart/checkout");
 ?>
 
 <div class="d-flex flex-row justify-content-center mt-4">
@@ -28,30 +30,28 @@ $totalShipping = 0;
                 <?php foreach ((array)$cartItems as $key => $value) : ?>
                     <?php if ($counter % 2) : ?>
                         <tr class="bg-light">
-                            <td><?php print_r($value['productManufacturer']); ?></td>
-                            <td><?php print_r($value['productName']); ?></td>
-                            <td><?php print_r($value['productSize']); ?></td>
-                            <td><?php print_r($value['productSellPrize']); ?></td>
-                            <td><?php print_r($value['productColor']); ?></td>
-                            <td><?php print_r($value['amount']); ?></td>
+                            <td><?= $value["productManufacturer"] ?></td>
+                            <td><?= $value["productName"] ?></td>
+                            <td><?= $value["productSize"] ?></td>
+                            <td><?= $value["productSellPrize"] ?></td>
+                            <td><?= $value["productColor"] ?></td>
+                            <td><?= $value['amount'] ?></td>
                             <th scope="row"><a href="<?= $url ?>/<?= $value['productID'] ?>">Mer information</a></th>
                         </tr>
                     <?php else : ?>
                         <tr>
-                            <td><?php print_r($value['productManufacturer']); ?></td>
-                            <td><?php print_r($value['productName']); ?></td>
-                            <td><?php print_r($value['productSize']); ?></td>
-                            <td><?php print_r($value['productSellPrize']); ?></td>
-                            <td><?php print_r($value['productColor']); ?></td>
-                            <td><?php print_r($value['amount']); ?></td>
+                            <td><?= $value["productManufacturer"] ?></td>
+                            <td><?= $value["productName"] ?></td>
+                            <td><?= $value["productSize"] ?></td>
+                            <td><?= $value["productSellPrize"] ?></td>
+                            <td><?= $value["productColor"] ?></td>
+                            <td><?= $value['amount'] ?></td>
                             <th scope="row"><a href="<?= $url ?>/<?= $value['productID'] ?>">Mer information</a></th>
                         </tr>
                     <?php endif; ?>
                     <?php
                     $price += ((int)$value['productSellPrize'] * (int)$value['amount']);
-                    $totalWeight += (int)$value['productWeight'] * (int)$value['amount'];
                     $amountOfItems += ((int)$value['amount']);
-                    $totalShipping = ($totalWeight / 1000) < 1 ? 50 : 50 + (20 * round($totalWeight / 1000));
                     $counter++;
                     ?>
                 <?php endforeach; ?>
@@ -59,11 +59,8 @@ $totalShipping = 0;
         </table>
         <?php if ($amountOfItems > 0) : ?>
             <p><b>Antal Produkter: <?= $amountOfItems ?></b></p>
-            <p><b>Total vikt: <?= round($totalWeight / 1000, 1) ?> kg</b></p>
             <p><b>Summa: <?= $price ?> kr</b></p>
-            <p><b>Frakt: <?= $totalShipping ?> kr</b></p>
-            <p><b>Summa totalt: <?= $price + $totalShipping ?> kr</b></p>
-            <button type="button" class="btn btn-primary w-10">Gå till kassan</button>
+            <a href="<?= $checkout ?>"><button type="button" class="btn btn-primary w-10">Gå till kassan</button></a>
         <?php elseif ($amountOfItems < 1) : ?>
             <p>Din kundvagn innehåller för tillfället inga produkter.</p>
         <?php endif; ?>
