a742a4107840870956c332fe6ad1e4465e7606bf
MagnusGreiff <mangegreiff@gmail.com>
2018-03-15 16:12:14 +0100

    Classes to handle Orders and OrderItems

---
+++ src/Order/Orders.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Course\Order;
+
+use \Anax\Database\ActiveRecordModel;
+
+class Orders extends ActiveRecordModel {
+    /**
+     * @var string $tableName name of the database table.
+     */
+    protected $tableName = "Orders";
+
+    protected $tableIdColumn = "orderID";
+
+    /**
+     * Columns in the table.
+     *
+     * @var integer $productID primary key auto incremented.
+     * @var string $productManufacturer not null.
+     * @var string $productName not null.
+     */
+
+    public $userID;
+    public $couponID;
+
+
+
+    public function setUserID($userID)
+    {
+        $this->userID = $userID;
+    }
+
+
+
+    public function setCouponID($couponID)
+    {
+        $this->couponID = $couponID;
+    }
+
+
+
+    public function getOrderID($userID)
+    {
+        $orderID = $this->find("userID", $userID);
+        return $orderID;
+    }
+
+
+
+    public function getLastInsertedID()
+    {
+        $orderID = $this->lastInsertId();
+        return $orderID;
+    }
+
+
+
+    public function getOrderByID($orderID)
+    {
+        return $this->find("orderID", $orderID);
+    }
+}
