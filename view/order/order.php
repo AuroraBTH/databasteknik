8590b60d197571241e76818cd4e66fa876f4154c
MagnusGreiff <mangegreiff@gmail.com>
2018-03-16 18:00:36 +0100

    Updated view

--- view/order/order.php
+++ view/order/order.php
@@ -4,6 +4,8 @@ namespace Anax\View;
 
 $ordersUrl = url("orders");
 $counter = 0;
+
+// var_dump($data);
 ?>
 
 <div class="d-flex flex-row justify-content-center mt-4">
@@ -12,24 +14,34 @@ $counter = 0;
             <thead>
                 <tr>
                     <th scope="col" class="border-bottom-0">Ordernummer</th>
-                    <th scope="col" class="border-bottom-0">Productnummer</th>
+                    <th scope="col" class="border-bottom-0">Produkt</th>
                     <th scope="col" class="border-bottom-0">Antal</th>
+                    <th scope="col" class="border-bottom-0">Pris</th>
+                    <th scope="col" class="border-bottom-0">Färg</th>
+                    <th scope="col" class="border-bottom-0">Tillverkare</th>
                     <th scope="col" class="border-bottom-0"></th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($data["orders"] as $item) : ?>
+                <?php foreach ($data['orderItems'] as $item) : ?>
+                    <!-- <?php var_dump($item['product']) ?> -->
                     <?php if ($counter % 2) : ?>
                         <tr class="bg-light">
-                            <td><?= $item->orderID ?></td>
-                            <td><?= $item->productID ?></td>
-                            <td><?= $item->productAmount ?></td>
+                            <td><?= $item['order']->orderID ?></td>
+                            <td><?= $item['product']->productName ?></td>
+                            <td><?= $item['order']->productAmount ?></td>
+                            <td><?= $item['product']->productSellPrize ?></td>
+                            <td><?= $item['product']->productColor ?></td>
+                            <td><?= $item['product']->productManufacturer ?></td>
                         </tr>
                     <?php else : ?>
                         <tr>
-                            <td><?= $item->orderID ?></td>
-                            <td><?= $item->productID ?></td>
-                            <td><?= $item->productAmount ?></td>
+                            <td><?= $item['order']->orderID ?></td>
+                            <td><?= $item['product']->productName ?></td>
+                            <td><?= $item['order']->productAmount ?></td>
+                            <td><?= $item['product']->productSellPrize ?></td>
+                            <td><?= $item['product']->productColor ?></td>
+                            <td><?= $item['product']->productManufacturer ?></td>
                         </tr>
                     <?php endif; ?>
                     <?php $counter++ ?>
