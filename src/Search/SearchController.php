3abb46c30800168ba8a703fe3e8017567fbbdf44
MagnusGreiff <mangegreiff@gmail.com>
2018-03-20 17:31:43 +0100

    Added searchController and abillity to render searchResult

---
+++ src/Search/SearchController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Course\Search;
+
+use \Anax\Configure\ConfigureInterface;
+use \Anax\Configure\ConfigureTrait;
+use \Anax\DI\InjectionAwareInterface;
+use \Anax\Di\InjectionAwareTrait;
+
+use \Course\Product\Product;
+
+
+class SearchController implements
+    ConfigureInterface,
+    InjectionAwareInterface
+{
+    use ConfigureTrait,
+    InjectionAwareTrait;
+
+
+
+    /**
+     * This function handles the rendering of aboutpage.
+     */
+    public function displayResult()
+    {
+        $title = "Sökresultat";
+        $view = $this->di->get("view");
+        $pageRender = $this->di->get("pageRender");
+
+        $url = $this->di->get("url");
+        $response = $this->di->get("response");
+
+        $db = $this->di->get("db");
+
+        $product = new Product;
+        $product->setDb($db);
+
+        if (isset($_POST["search"])) {
+            $searchString = htmlspecialchars($_POST["search"]);
+            $searchResult = $product->searchProducts($searchString);
+            $searchResultCount = count($searchResult);
+        } else if (!isset($_POST["search"])) {
+            $redirectUrl = $url->create("");
+            $response->redirect($redirectUrl);
+            return false;
+        }
+
+        $data = [
+            "searchResultCount" => $searchResultCount,
+            "searchResult" => $searchResult
+        ];
+
+        $view->add("search/search", $data);
+        $pageRender->renderPage(["title" => $title]);
+    }
+}
