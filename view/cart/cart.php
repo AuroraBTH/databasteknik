343ee6d21f1935025262daba002953ed94efe25e
MagnusGreiff <mangegreiff@gmail.com>
2018-03-20 21:00:28 +0100

    Added remove from cart buttons

--- view/cart/cart.php
+++ view/cart/cart.php
@@ -15,70 +15,74 @@ $checkout = url("cart/checkout");
 <div class="d-flex flex-row justify-content-center mt-4">
     <div class="w-75">
         <h1 class="text-center">Kundvagn</h1>
-        <table class="table border mb-4">
-            <thead>
-                <tr>
-                    <th scope="col" class="border-bottom-0">Tillverkare</th>
-                    <th scope="col" class="border-bottom-0">Namn</th>
-                    <th scope="col" class="border-bottom-0">Storlek</th>
-                    <th scope="col" class="border-bottom-0">Pris</th>
-                    <th scope="col" class="border-bottom-0">Färg</th>
-                    <th scope="col" class="border-bottom-0">Antal</th>
-                    <th scope="col" class="border-bottom-0"></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ((array)$cartItems as $key => $value) : ?>
-                    <?php if ($counter % 2) : ?>
-                        <tr class="bg-light">
-                            <td><?= $value["productManufacturer"] ?></td>
-                            <td><?= $value["productName"] ?></td>
-                            <td><?= $value["productSize"] ?></td>
-                            <td><?= $value["productSellPrize"] ?></td>
-                            <td><?= $value["productColor"] ?></td>
-                            <td><?= $value['amount'] ?></td>
-                            <th scope="row"><a href="<?= $url ?>/<?= $value['productID'] ?>">Mer information</a></th>
-                        </tr>
-                    <?php else : ?>
-                        <tr>
-                            <td><?= $value["productManufacturer"] ?></td>
-                            <td><?= $value["productName"] ?></td>
-                            <td><?= $value["productSize"] ?></td>
-                            <td><?= $value["productSellPrize"] ?></td>
-                            <td><?= $value["productColor"] ?></td>
-                            <td><?= $value['amount'] ?></td>
-                            <th scope="row"><a href="<?= $url ?>/<?= $value['productID'] ?>">Mer information</a></th>
-                        </tr>
-                    <?php endif; ?>
-                    <?php
-                    $price += ((int)$value['productSellPrize'] * (int)$value['amount']);
-                    $amountOfItems += ((int)$value['amount']);
-                    $counter++;
-                    ?>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-        <?php if ($amountOfItems > 0) : ?>
-            <table class="table w-25 border border-top-0">
+        <div id="cartView">
+            <table class="table border mb-4">
                 <thead>
-                    <tr class="text-center font-weight-bold">
-                        <th colspan="2">Beställningsinformation</th>
+                    <tr>
+                        <th scope="col" class="border-bottom-0">Tillverkare</th>
+                        <th scope="col" class="border-bottom-0">Namn</th>
+                        <th scope="col" class="border-bottom-0">Storlek</th>
+                        <th scope="col" class="border-bottom-0">Pris</th>
+                        <th scope="col" class="border-bottom-0">Färg</th>
+                        <th scope="col" class="border-bottom-0">Antal</th>
+                        <th scope="col" class="border-bottom-0"></th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th>Antal Produkter:</th>
-                        <td><?= $amountOfItems ?></td>
-                    </tr>
-                    <tr>
-                        <th>Summa:</th>
-                        <td><?= $price ?> kr</td>
-                    </tr>
+                    <?php foreach ((array)$cartItems as $key => $value) : ?>
+                        <?php if ($counter % 2) : ?>
+                            <tr class="bg-light">
+                                <td><?= $value["productManufacturer"] ?></td>
+                                <td><?= $value["productName"] ?></td>
+                                <td><?= $value["productSize"] ?></td>
+                                <td><?= $value["productSellPrize"] ?></td>
+                                <td><?= $value["productColor"] ?></td>
+                                <td><?= $value['amount'] ?></td>
+                                <th scope="row"><a href="<?= $url ?>/<?= $value['productID'] ?>">Mer information</a></th>
+                                <td><button class="removeButton" onclick="removeFromCart('<?= $value["productID"] ?>')" type="button" name="button"><i class="fas fa-times"></i></button></td>
+                            </tr>
+                        <?php else : ?>
+                            <tr>
+                                <td><?= $value["productManufacturer"] ?></td>
+                                <td><?= $value["productName"] ?></td>
+                                <td><?= $value["productSize"] ?></td>
+                                <td><?= $value["productSellPrize"] ?></td>
+                                <td><?= $value["productColor"] ?></td>
+                                <td><?= $value['amount'] ?></td>
+                                <th scope="row"><a href="<?= $url ?>/<?= $value['productID'] ?>">Mer information</a></th>
+                                <td><button class="removeButton" onclick="removeFromCart('<?= $value["productID"] ?>')" type="button" name="button"><i class="fas fa-times"></i></button></td>
+                            </tr>
+                        <?php endif; ?>
+                        <?php
+                        $price += ((int)$value['productSellPrize'] * (int)$value['amount']);
+                        $amountOfItems += ((int)$value['amount']);
+                        $counter++;
+                        ?>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
-            <a href="<?= $checkout ?>"><button type="button" class="btn btn-primary w-10">Gå till kassan</button></a>
-        <?php elseif ($amountOfItems < 1) : ?>
-            <p>Din kundvagn innehåller för tillfället inga produkter.</p>
-        <?php endif; ?>
+            <?php if ($amountOfItems > 0) : ?>
+                <table class="table w-25 border border-top-0">
+                    <thead>
+                        <tr class="text-center font-weight-bold">
+                            <th colspan="2">Beställningsinformation</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Antal Produkter:</th>
+                            <td><?= $amountOfItems ?></td>
+                        </tr>
+                        <tr>
+                            <th>Summa:</th>
+                            <td><?= $price ?> kr</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <a href="<?= $checkout ?>"><button type="button" class="btn btn-primary w-10">Gå till kassan</button></a>
+            <?php elseif ($amountOfItems < 1) : ?>
+                <p>Din kundvagn innehåller för tillfället inga produkter.</p>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
