a6a42c5ff995e8d1f3d48198cfa9f1673012b013
MagnusGreiff <mangegreiff@gmail.com>
2018-03-21 08:05:39 +0100

    Added button that will remove all products from cart when clicked

--- view/cart/cart.php
+++ view/cart/cart.php
@@ -62,6 +62,7 @@ $checkout = url("cart/checkout");
                 </tbody>
             </table>
             <?php if ($amountOfItems > 0) : ?>
+                <button class="btn btn-primary w-10 mb-4" onclick=removeAllFromCart() type="button" name="button">Töm kundvagn</button>
                 <table class="table w-25 border border-top-0">
                     <thead>
                         <tr class="text-center font-weight-bold">
