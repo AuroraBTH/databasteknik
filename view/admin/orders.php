d72fad8eb92cc3df8a1aafcedf72be3626f04cbb
MagnusGreiff <mangegreiff@gmail.com>
2018-03-26 14:02:51 +0200

    Removed duplicate code

--- view/admin/orders.php
+++ view/admin/orders.php
@@ -19,19 +19,11 @@ $counter = 0;
             </thead>
             <tbody>
                 <?php foreach ($data["orders"] as $item) : ?>
-                    <?php if ($counter % 2) : ?>
-                        <tr class="bg-light">
+                    <tr <?= ($counter % 2) == 0 ? 'class="bg-light"' : "" ?>>
                             <td><?= $item->orderID ?></td>
                             <td><?= $item->purchaseTime ?></td>
                             <th scope="row"><a href="<?= $adminURL ?>/order/<?= $item->orderID ?>">Mer information</a></th>
                         </tr>
-                    <?php else : ?>
-                        <tr>
-                            <td><?= $item->orderID ?></td>
-                            <td><?= $item->purchaseTime ?></td>
-                            <th scope="row"><a href="<?= $adminURL ?>/order/<?= $item->orderID ?>">Mer information</a></th>
-                        </tr>
-                    <?php endif; ?>
                     <?php $counter++ ?>
                 <?php endforeach; ?>
             </tbody>
--- view/admin/products.php
+++ view/admin/products.php
@@ -23,8 +23,7 @@ $admin = url("admin")
             </thead>
             <tbody>
                 <?php foreach ($data["products"] as $item) : ?>
-                    <?php if ($counter % 2) : ?>
-                        <tr class="bg-light">
+                    <tr <?= ($counter % 2) == 0 ? 'class="bg-light"' : "" ?>>
                             <td><?= $item->productManufacturer ?></td>
                             <td><?= $item->productName ?></td>
                             <td><?= $item->productSize ?></td>
@@ -32,16 +31,6 @@ $admin = url("admin")
                             <td><?= $item->productColor ?></td>
                             <th scope="row"><a href="<?= $url ?>/<?= $item->productID ?>">Mer information</a></th>
                         </tr>
-                    <?php else : ?>
-                        <tr>
-                            <td><?= $item->productManufacturer ?></td>
-                            <td><?= $item->productName ?></td>
-                            <td><?= $item->productSize ?></td>
-                            <td><?= $item->productSellPrize ?></td>
-                            <td><?= $item->productColor ?></td>
-                            <th scope="row"><a href="<?= $url ?>/<?= $item->productID ?>">Mer information</a></th>
-                        </tr>
-                    <?php endif; ?>
                     <?php $counter++ ?>
                 <?php endforeach; ?>
             </tbody>
