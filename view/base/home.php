b2d35f78b75feeb10e6456b5c76ba4d18cf44e75
MagnusGreiff <mangegreiff@gmail.com>
2018-03-26 14:20:58 +0200

    Third fix for codacy

--- view/base/home.php
+++ view/base/home.php
@@ -26,7 +26,7 @@ $genderCounter = 0;
                             </tr>
                         </thead>
                         <tbody>
-                            <?php foreach ($top10 as $item): ?>
+                            <?php foreach ($top10 as $item) : ?>
                                 <tr <?= ($counter % 2) == 0 ? 'class="bg-light"' : "" ?>>
                                     <td><?= $item['productManufacturer'] ?></td>
                                     <td><?= $item['productName'] ?></td>
