4a7db365aaaa99dbd746f73d4ce1efbba062862a
Niklas Andersson <nhdandersson@gmail.com>
2018-04-06 16:13:38 +0200

    Added source link to footer.

--- view/defaults/footer.php
+++ view/defaults/footer.php
@@ -1,6 +1,6 @@
 <footer class="d-flex justify-content-center bg-light border border-left-0 border-right-0 border-bottom-0">
     <div class="p-4 m-4 w-75">
-        <p>Detta projekt material utvecklas för kursen Databasteknik</p>
+        <p>Detta projekt material utvecklas för kursen Databasteknik (<a href="#" target="_blank">källkod <i class="fas fa-external-link-alt"></i></a>).</p>
         <p>Underhålls utav <a href="https://github.com/AuroraBTH" target="_blank">Niklas Andersson</a> och <a href="https://github.com/MagnusGreiff" target="_blank">Magnus Greiff</a>.</p>
     </div>
 </footer>
