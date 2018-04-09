b34d314dbe1aedf199f4b726226342c85b727921
MagnusGreiff <mangegreiff@gmail.com>
2018-04-09 16:07:46 +0200

    Fixed _blank exploit

--- view/defaults/footer.php
+++ view/defaults/footer.php
@@ -1,10 +1,14 @@
 <footer class="d-flex justify-content-center bg-light border border-left-0 border-right-0 border-bottom-0">
     <div class="p-4 m-4 w-75">
         <p>Detta projekt material utvecklas för kursen Databasteknik
-            ( <a href="https://github.com/aurora-munge/databasteknik" target="_blank">källkod
+            ( <a href="https://github.com/aurora-munge/databasteknik" target="_blank" rel="noopener noreferrer">källkod
                 <i class="fas fa-external-link-alt"></i>
             </a>).
         </p>
-        <p>Underhålls utav <a href="https://github.com/AuroraBTH" target="_blank">Niklas Andersson</a> och <a href="https://github.com/MagnusGreiff" target="_blank">Magnus Greiff</a>.</p>
+        <p>Underhålls utav
+            <a href="https://github.com/AuroraBTH" target="_blank" rel="noopener noreferrer">Niklas Andersson</a>
+            och
+            <a href="https://github.com/MagnusGreiff" target="_blank" rel="noopener noreferrer">Magnus Greiff</a>.
+        </p>
     </div>
 </footer>
