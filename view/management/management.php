5eb66106349bc3a90265c7ed1d78f2abb37bae66
MagnusGreiff <mangegreiff@gmail.com>
2018-04-09 15:31:48 +0200

    Updated title and disabled profit

--- view/management/management.php
+++ view/management/management.php
@@ -10,7 +10,7 @@ $management = url("management");
 
 <div class="d-flex flex-row justify-content-center p-2">
     <div class="home w-75">
-        <p class="h2 text-center mt-4">Kontrollpanel Admin</p>
+        <p class="h2 text-center mt-4">Kontrollpanel Management</p>
         <div class="row text-center mr-0 ml-0">
                 <div class="w-50 mt-3 mb-3 col-md-<?php echo $bootstrapColWidth; ?>">
                     <a class="mx-auto w-75 btn btn-lg btn-primary pt-4 pb-4 btn-block"
@@ -22,7 +22,7 @@ $management = url("management");
                 </div>
                 <div class="w-50 mt-3 mb-3 col-md-<?php echo $bootstrapColWidth; ?>">
                     <a class="mx-auto w-75 btn btn-lg btn-primary pt-4 pb-4 btn-block"
-                    href="<?= $management ?>/profit">Högst vinst</a>
+                    href="#">Högst vinst</a>
                 </div>
         </div>
     </div>
