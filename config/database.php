d558ea0395d01f1f2292997cae646ed10da011d5
Niklas Aurora Andersson <nhdandersson@gmail.com>
2019-10-10 16:20:06 +0200

    Added default Docker MySQL settings

--- config/database.php
+++ config/database.php
@@ -12,17 +12,17 @@
  *  "dsn" => "sqlite:memory::",
  */
 return [
-    "dsn"             => "sqlite:" . ANAX_INSTALL_PATH . "/data/db.sqlite",
-    "username"        => null,
-    "password"        => null,
-    "driver_options"  => null,
-    "fetch_mode"      => \PDO::FETCH_OBJ,
-    "table_prefix"    => null,
+    "dsn"             => "mysql:host=itsec-anax-db;dbname=itsec",
+    "username"        => "root",
+    "password"        => "example",
+    "driver_options"  => [
+        \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
+    ],
     "session_key"     => "Anax\Database",
 
     // True to be very verbose during development
     "verbose"         => null,
 
     // True to be verbose on connection failed
-    "debug_connect"   => null,
+    "debug_connect"   => true,
 ];
