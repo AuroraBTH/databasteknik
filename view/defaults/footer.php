38982ebfe2564c5fe7d32bac5613eccfd67db60d
Niklas Aurora Andersson <nhdandersson@gmail.com>
2019-10-22 15:17:48 +0200

    Updated credits

--- view/defaults/footer.php
+++ view/defaults/footer.php
@@ -1,14 +1,11 @@
 <footer class="d-flex justify-content-center bg-light border border-left-0 border-right-0 border-bottom-0">
-    <div class="p-4 m-4 w-75">
-        <p>Detta projekt material utvecklas för kursen Databasteknik
-            ( <a href="https://github.com/aurora-munge/databasteknik" target="_blank" rel="noopener noreferrer">källkod
-                <i class="fas fa-external-link-alt"></i>
-            </a>).
-        </p>
-        <p>Underhålls utav
-            <a href="https://github.com/AuroraBTH" target="_blank" rel="noopener noreferrer">Niklas Andersson</a>
+    <div class="p-4 m-4 d-flex flex-column justify-content-center">
+        <p>
+            Underhålls utav <a href="https://github.com/dbwebb-se" target="_blank" rel="noopener noreferrer">dbwebb</a>.
+            Tidigare ansvariga <a href="https://github.com/AuroraBTH" target="_blank" rel="noopener noreferrer">Niklas Andersson</a>
             och
             <a href="https://github.com/MagnusGreiff" target="_blank" rel="noopener noreferrer">Magnus Greiff</a>.
         </p>
+        <p>En del utav kursmaterialet i DV1616, Informationssäkerhet med webbtillämpningar.</p>
     </div>
 </footer>
